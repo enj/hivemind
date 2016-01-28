bfe9e6dbdf146ffaccc30fcd1d0f7dc9417548e8
srbaucom <srbaucom@ncsu.edu>
2016-01-28 13:07:12 -0500

    worker.py had been empty. I think I pushed without saving it. Now has code

--- Team29/MPI/worker.py
+++ Team29/MPI/worker.py
@@ -1,3 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+class Worker(object):
+    def __init__(self, node):
+        self.node = node
+
+    def run(self, task):
+        task.run()
+        return task.next
