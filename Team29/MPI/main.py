76db478bcfb476f72bd3c18f4eb6d8006d164962
srbaucom <srbaucom@ncsu.edu>
2016-01-29 17:11:02 -0500

    added Sarah's test bin

--- Team29/MPI/main.py
+++ Team29/MPI/main.py
@@ -10,19 +10,23 @@ from master import Master
 from worker import Worker
 from queue import TaskQueue
 
-a = Task("/Users/srbaucom/Documents1", "a.out")
-b = Task("/Users/srbaucom/Documents2", "a.out")
-c = Task("/Users/srbaucom/Documents3", "a.out")
-p = Pipeline(a, b, c)
-q = TaskQueue(p)
+s1 = Task("/pvfs2/srbaucom/bin", "app", "This ")
+s2 = Task("/pvfs2/srbaucom/bin", "app", "is ")
+s3 = Task("/pvfs2/srbaucom/bin", "app", "a ")
+s4 = Task("/pvfs2/srbaucom/bin", "app", "test ")
+s5 = Task("/pvfs2/srbaucom/bin", "app", "sentence.")
+sp = Pipeline(s1, s2, s3, s4, s5)
+
+
+q = TaskQueue(sp)
 
 rank = MPI.COMM_WORLD.rank
 
 if rank == MASTER:
-	m = Master(MPI, q)
-	while m.completed_tasks != m.num_tasks:
-		m.receive()
+    m = Master(MPI, q)
+    while m.completed_tasks != m.num_tasks:
+        m.receive()
 else:
-	w = Worker(MPI)
-	while w.tag != tags.EXIT:
-		w.receive()
+    w = Worker(MPI)
+    while w.tag != tags.EXIT:
+        w.receive()
