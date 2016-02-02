b8225dc2078dc4dc22c4446c9f92393f9eb7ed29
Monis Khan <mmkhan2@ncsu.edu>
2016-02-02 14:41:43 -0500

    Started refactoring and documentation.

--- Team29/MPI/task.py
+++ Team29/MPI/task.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+
+"""Represents a Task in a pipeline."""
 
 from subprocess import call
 
 
 class Task(object):
+    """A Task is the smallest unit of work in a pipeline."""
+
     def __init__(self, exe_path, exe, *args):
+        """Construct a task based on the given path, executable, and arguments.
+
+        :param exe_path: The file system location of the executable
+        :type exe_path: string
+        :param exe: The name of the executable
+        :type exe: string
+        :param args: The list of parameters needed to the run the executable
+        :type args: iterable of strings
+        """
         self.exe_path = exe_path
         self.exe = "./" + exe
         self.args = list(args)
         self.next = None
 
     def run(self):
+        """TODO."""
         call([self.exe] + self.args, cwd=self.exe_path)
 
     def link(self, next):
+        """TODO."""
         self.next = next
--- Team29/MPI/util.py
+++ Team29/MPI/util.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
-# encoding: utf-8
+# -*- coding: utf-8 -*-
+
+
+"""Represents utility functions."""
+
 
 def enum(*sequential, **named):
-    """Handy way to fake an enumerated type in Python
+    """Handy way to fake an enumerated type in Python.
+
     http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+
+    :param sequential: the values of the enum (required)
+    :type sequential: iterable
+    :param named: used to do reverse mapping from value to enum (optional)
+    :type named: dictionary
+    :returns: a new enum type based on the input
     """
     enums = dict(zip(sequential, range(len(sequential))), **named)
     return type('Enum', (), enums)
 
+# Each of the different states a worker can be in
 tags = enum('READY', 'DONE', 'EXIT', 'START', 'WAIT')
+
+# The MPI rank of the master
 MASTER = 0
