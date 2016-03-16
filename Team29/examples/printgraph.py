cbfe69edf74b464ef5097c2e33efbbb60c5c76f6
Sarah Baucom <srbaucom@ncsu.edu>
2016-03-16 16:12:21 -0400

    Added DNA JSON and CSV files.

--- Team29/examples/printgraph.py
+++ Team29/examples/printgraph.py
@@ -10,13 +10,13 @@ from hivemind.util import json_to_tasks, read_csv
 from datagenerator import DataGenerator
 
 
-tasks = json_to_tasks('sarah.json')
+tasks = json_to_tasks('dna.json')
 
 dg = DataGenerator()
-#framework = PipelineFramework(tasks)
-framework = dg.get_loose_pipeline()
+framework = PipelineFramework(tasks)
+#framework = dg.get_loose_pipeline()
 
-patients = read_csv('sarah.csv')
+patients = read_csv('dna.csv')
 concrete_pipelines = []
 for i, row in enumerate(patients):
     concrete_pipelines.append(ConcretePipeline(i, framework, row, ""))
@@ -25,11 +25,11 @@ p = concrete_pipelines[0]
 
 # Need to create a layout when doing
 # separate calls to draw nodes and edges
-#pos = nx.spring_layout(G)
-#pos = nx.circular_layout(G)
-#pos = nx.random_layout(G)
-#pos = nx.shell_layout(G)
-pos = nx.fruchterman_reingold_layout(p.dag, scale=1)
+pos = nx.spring_layout(p.dag)
+#pos = nx.circular_layout(p.dag)
+#pos = nx.random_layout(p.dag)
+#pos = nx.shell_layout(p.dag)
+#pos = nx.fruchterman_reingold_layout(p.dag)
 
 
 edge_colours = ['black' for edge in p.dag.edges()]
