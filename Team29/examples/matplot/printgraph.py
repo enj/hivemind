09f558ab8e2fd7d4276777e15358834874b9c3f0
Sarah Baucom <srbaucom@ncsu.edu>
2016-04-22 14:07:23 -0400

    More cleanup

--- Team29/examples/matplot/printgraph.py
+++ Team29/examples/matplot/printgraph.py
@@ -1,34 +1,22 @@
 import networkx as nx
-#import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import NullLocator
-import cPickle as pickle
 from networkx.drawing.nx_agraph import graphviz_layout
 
-from hivemind.pipeline import Task
 from hivemind.pipeline import PipelineFramework, ConcretePipeline
 from hivemind.util import json_to_tasks, read_csv
 
-from datagenerator import DataGenerator
 
-from sys import stdout
-from networkx.drawing.nx_pydot import write_dot
+tasks = json_to_tasks('../../tests/blackbox/sarah_pipeline.json')
+patients = read_csv('../dna/dna.csv')
 
-
-tasks = json_to_tasks('sarah_pipeline.json')
-
-dg = DataGenerator()
 framework = PipelineFramework(tasks)
-#framework = dg.get_loose_pipeline()
 
-patients = read_csv('dna/dna.csv')
 concrete_pipelines = []
 for i, row in enumerate(patients):
     concrete_pipelines.append(ConcretePipeline(i, framework, row, ""))
 
 p = concrete_pipelines[0]
-
-#print p.dag.nodes()
 
 G = nx.DiGraph()
 for task in p.dag.nodes():
@@ -36,30 +24,18 @@ for task in p.dag.nodes():
     for successor in p.dag.successors(task):
         G.add_edge(task._uid, successor._uid)
 
-# Need to create a layout when doing
-# separate calls to draw nodes and edges
-#pos = nx.spring_layout(p.dag)
-#pos = nx.circular_layout(p.dag)
-#pos = nx.random_layout(p.dag)
-#pos = nx.shell_layout(p.dag)
-#pos = nx.fruchterman_reingold_layout(p.dag)
+# Need to create a layout
 pos = graphviz_layout(G, prog='dot')
-#pos = nx.layout.collections(p.dag)
-
-
 
 edge_colours = ['black' for edge in G.edges()]
 black_edges = [edge for edge in G.edges()]
 nx.draw_networkx_nodes(G, pos, node_shape='o', node_color=range(G.number_of_nodes()), linewidths=0.5, cmap=plt.cm.Paired, node_size=100)
 nx.draw_networkx_edges(G, pos, width=.25, arrows=True)
 nx.draw_networkx_labels(G, pos, font_size=4)
-#nx.draw_networkx_labels(p.dag, pos, {task : str(task._uid) + "," + str(task._pid) for task in p.dag.nodes()}, font_size=12)
 
 plt.axis("off")
 plt.gca().xaxis.set_major_locator(NullLocator())
 plt.gca().yaxis.set_major_locator(NullLocator())
-plt.margins(0,0)
-#plt.figure(dpi=200)
-#plt.show()
+plt.margins(0, 0)
+
 plt.savefig("graph.png", bbox_inches='tight', pad_inches=0, dpi=200)
-#write_dot(G, stdout)
